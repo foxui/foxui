9665b4a8d5d902b186988d4b278d99e89fda6b58
meng <30962088@qq.com>
2014-07-17 18:21:25 +0800

    update

---
+++ examples/sofind/js/app.js
@@ -0,0 +1,12 @@
+rivets.formatters.indexPlus = function(index) {
+	return index + 1;
+};
+window.addEventListener('HTMLImportsLoaded', function(e) {
+	document.body.style.visibility = "visible";
+});
+
+
+$(function() {
+    FastClick.attach(document.body);
+});
+
