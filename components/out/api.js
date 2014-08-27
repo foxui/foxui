01528bf3dbd325bcf9bdb1d061b571f3fd97079c
Cyndi <cyndi.ai76@gmail.com>
2014-08-27 10:51:20 +0800

    website

---
+++ components/out/api.js
@@ -0,0 +1,67 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "buildData",
+        "fox-actionsheet",
+        "fox-ajax",
+        "fox-calendar",
+        "fox-content",
+        "fox-css",
+        "fox-dialog",
+        "fox-image-lazyload",
+        "fox-image-slider"
+    ],
+    "modules": [
+        "fox-actionsheet",
+        "fox-ajax",
+        "fox-calendar",
+        "fox-content",
+        "fox-dialog",
+        "fox-icon",
+        "fox-image-lazyload",
+        "fox-image-slider"
+    ],
+    "allModules": [
+        {
+            "displayName": "fox-actionsheet",
+            "name": "fox-actionsheet",
+            "description": "This is the actionsheet component that the users can input any values to be appearred as \ndifferent types of clickable button on the bottom of the screen."
+        },
+        {
+            "displayName": "fox-ajax",
+            "name": "fox-ajax",
+            "description": "This is the ajax component that enables the users to easily manage and access\nto different databases through ajax."
+        },
+        {
+            "displayName": "fox-calendar",
+            "name": "fox-calendar",
+            "description": "This is the fox-calendar constructor function"
+        },
+        {
+            "displayName": "fox-content",
+            "name": "fox-content",
+            "description": "This is the fox-content constructor function"
+        },
+        {
+            "displayName": "fox-dialog",
+            "name": "fox-dialog",
+            "description": "This is the dialog component that allows users to easily create pop-up dialogs"
+        },
+        {
+            "displayName": "fox-icon",
+            "name": "fox-icon",
+            "description": "This is the fox-icon constructor function"
+        },
+        {
+            "displayName": "fox-image-lazyload",
+            "name": "fox-image-lazyload",
+            "description": "This method is called to set up the lazyload"
+        },
+        {
+            "displayName": "fox-image-slider",
+            "name": "fox-image-slider",
+            "description": "This is the fox-image-slider's helper method in order to build up the data"
+        }
+    ]
+} };
+});
